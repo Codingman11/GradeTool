ca03bddec2001ac38fbf6012a472f432704d8f33
JessePel <jesse1.peltola@outlook.com>
2023-02-13 12:33:48 +0200

    Data classes added

--- src/GradingTool.py
+++ src/GradingTool.py
@@ -1,23 +1,33 @@
 import dearpygui.dearpygui as dpg
 import GradingToolGUI as gui
-dpg.create_context()
-dpg.create_viewport(title="Grading tool", width=1200, height=600, resizable=True, always_on_top=True)
-dpg.setup_dearpygui()
-
-default_font, hl_font = gui.initialize_font()
-
-with dpg.window(label="Opiskelijat", pos=(0,0)):
-    
-    button1  = dpg.add_button(label="TESTI")
-    dpg.bind_font(hl_font)
 
 
 
+def main() -> None:
 
-dpg.show_viewport(maximized=True)
+
+    dpg.create_context()
+    dpg.create_viewport(title="Grading tool", width=1200, height=600, resizable=True, always_on_top=True)
+    dpg.setup_dearpygui()
+
+    default_font, hl_font = gui.initialize_font()
+
+    with dpg.window(label="Opiskelijat", pos=(0,0)):
+        
+        button1  = dpg.add_button(label="TESTI")
+        dpg.bind_font(hl_font)
 
 
-while dpg.is_dearpygui_running():
-    dpg.render_dearpygui_frame()
 
-dpg.destroy_context()
+
+    dpg.show_viewport(maximized=True)
+
+
+    while dpg.is_dearpygui_running():
+        dpg.render_dearpygui_frame()
+
+    dpg.destroy_context()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/data.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+
+@dataclass
+class StudentInfo:
+    def __init__(self, name: str, student_number: str, exam_level: str, group: str, feedback: []) -> None:
+        self.name = name
+        self.student_name = student_number
+        self.exam_level = exam_level
+        self.group = group
+        self.feedback = feedback
+@dataclass
+class ErrorInfo:
+    def __init__(self, error:tuple, text:str, errorpoints:float, amount:int, alternative:list, exclude:list) -> None:
+        self.error = error
+        self.text = text
+        self.errorpoints = errorpoints
+        self.amount = amount
+        self.alternative = alternative
+        self.exclude = exclude
+@dataclass
+class Category:
+    def __init__(self, name:str, category_sum:int) -> None:
+        self.name = name
+        self.category_sum = category_sum
+
